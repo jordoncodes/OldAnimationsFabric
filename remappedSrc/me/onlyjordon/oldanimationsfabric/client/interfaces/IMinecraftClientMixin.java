96a52ffbfdb3703c6a6a9b08b81345be087809be
Jordon <98439832+jordoncodes@users.noreply.github.com>
2024-02-06 19:58:17 +1000

    bug fix

--- remappedSrc/me/onlyjordon/oldanimationsfabric/client/interfaces/IMinecraftClientMixin.java
+++
@@ -1,8 +0,0 @@
-package me.onlyjordon.oldanimationsfabric.client.interfaces;
-
-import net.minecraft.client.network.ClientPlayerEntity;
-
-public interface IMinecraftClientMixin {
-
-    void attemptSwing(ClientPlayerEntity player);
-}
